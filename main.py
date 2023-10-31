7877c1ce5b13712de476a5e5138d5f6d9018237e
ピクセルGM <35326620+PixelGM@users.noreply.github.com>
2023-10-31 09:48:56 -0700

    Grayscale Camera
    Press 'q' to exit the app.

--- main.py
+++ main.py
@@ -1,1 +1,24 @@
-print('Hello World! :3')
+import cv2
+
+# Initialize camera
+cap = cv2.VideoCapture(0)
+while True:
+    # Capture frame-by-frame
+    ret, frame = cap.read()
+
+    # Convert the frame to grayscale
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+    # Display the grayscale frame
+    cv2.imshow('Grayscale Video', gray)
+
+    # Wait for a key event for 1 millisecond
+    key = cv2.waitKey(1)
+
+    # Press 'q' to close the window
+    if key & 0xFF == ord('q'):
+        break
+
+# Release the camera and close all windows
+cap.release()
+cv2.destroyAllWindows()
