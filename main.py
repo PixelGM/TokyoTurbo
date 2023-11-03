b5e787cd830f8804184f577a750be5e1c2784413
ピクセルGM <35326620+PixelGM@users.noreply.github.com>
2023-11-02 17:15:04 -0700

    Detect Whitest Point

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 import cv2
+import numpy as np
 
 # Initialize camera
 cap = cv2.VideoCapture(0)
+
 while True:
     # Capture frame-by-frame
     ret, frame = cap.read()
@@ -9,8 +11,14 @@ while True:
     # Convert the frame to grayscale
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
-    # Display the grayscale frame
-    cv2.imshow('Grayscale Video', gray)
+    # Find the location of the minimum pixel value in the grayscale frame
+    (minVal, maxVal, minLoc, maxLoc) = cv2.minMaxLoc(gray)
+
+    # Mark the location of the minimum pixel value on the original frame
+    cv2.circle(frame, maxLoc, 5, (0, 255, 0), 2)
+
+    # Display the marked frame
+    cv2.imshow('Whitest Point', frame)
 
     # Wait for a key event for 1 millisecond
     key = cv2.waitKey(1)
