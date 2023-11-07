f581560be907a931be5c549b74c3619def2b200a
ピクセルGM <35326620+PixelGM@users.noreply.github.com>
2023-11-06 20:19:43 -0800

    Input and Output Test (In Console)

--- dew_detector.py
+++ dew_detector.py
@@ -1,31 +1,35 @@
 import math
 
+# Function to calculate the dew point
 def calculate_dew_point(T_ambient, H_ambient):
-    # Constants for dew point calculation
     a = 17.27
     b = 237.7
-    # Calculate the dew point using the formula
     alpha = ((a * T_ambient) / (b + T_ambient)) + math.log(H_ambient/100.0)
     T_dew = (b * alpha) / (a - alpha)
     return T_dew
 
+# Function to detect dew formation
 def dew_formation_detector(T_ambient, T_windshield, H_ambient):
-    # Calculate the dew point
     T_dew = calculate_dew_point(T_ambient, H_ambient)
-    
-    # Determine if dew is likely to form on the windshield
-    if T_windshield <= T_dew:
-        return True
-    else:
-        return False
+    return T_windshield <= T_dew
 
-# Inputs
-T_ambient = 10.0      # Ambient temperature in degrees Celsius
-T_windshield = 8.0    # Windshield temperature in degrees Celsius
-H_ambient = 85.0      # Ambient humidity in percent
+# Main function to handle dew point detection
+def main():
+    try:
+        # Get values from user input and convert them to float
+        T_ambient = float(input('Enter ambient temperature (°C): '))
+        T_windshield = float(input('Enter windshield temperature (°C): '))
+        H_ambient = float(input('Enter ambient humidity (%): '))
 
-# Perform the dew detection
-dew_detected = dew_formation_detector(T_ambient, T_windshield, H_ambient)
+        # Perform the dew detection
+        dew_detected = dew_formation_detector(T_ambient, T_windshield, H_ambient)
 
-# Print the result
-print("Dew formation detected:" if dew_detected else "No dew formation detected.")
+        # Output the result message
+        dew_message = "Dew formation detected." if dew_detected else "No dew formation detected."
+        print(dew_message)
+    except ValueError as e:
+        # Output the error message if conversion to float fails
+        print("Error: Please enter valid numbers for all fields.")
+
+if __name__ == "__main__":
+    main()
